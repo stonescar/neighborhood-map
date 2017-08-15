5e2a3cb7c779b504dd78eac9f6a02dd22714ffdb
Steinar Utstrand <stonescar@gmail.com>
2017-08-15 15:03:27 +0200

    move modal functions to func.js and add comments

--- js/func.js
+++ js/func.js
@@ -1,3 +1,5 @@
+// Page functionality functions
+
 //Clear active spot when closing modal
 $(document).on('hide.bs.modal', '#infoModal', function() {
     my.viewModel.clearActiveSpot();
@@ -14,7 +16,6 @@ $(window).on('resize', function() {
 // When document is loaded
 $(document).ready(function() {
     collapseOnMobile()
-
     // Initialize perfect scrollbar on list group
     $('.list-group').perfectScrollbar({suppressScrollX: true, wheelSpeed: 0.5});
 });
@@ -25,5 +26,132 @@ function collapseOnMobile() {
         $('#spots-menu').collapse('hide');
     } else {
         $('#spots-menu').collapse('show');
+    }
+}
+
+function openModal() {
+    // If information has not been fetched before, call ajax functions
+    // Then, populate and show modal
+    if (!infoWindow.marker.detailed) {
+        getWiki(infoWindow.marker);
+        getFlickr(infoWindow.marker, populateDomFlickr);
+        getFsTips(infoWindow.marker, populateDomFsTip);
+        getFsVenueDetails(infoWindow.marker, populateDomFsVenueDetails);
+        infoWindow.marker.detailed = true;
+    } else {
+        populateDomFlickr()
+        populateDomFsTip();
+        populateDomFsVenueDetails();
+    }
+    showModal();
+
+function showModal() {
+    // Hide infowindow
+    infoWindow.setMap(null);
+    infoWindow.marker = null;
+    // Make sure the marker is stored on the active `spot` object
+    if (!my.viewModel.activeSpot().marker()) {
+        my.viewModel.activeSpot().marker(markers[my.viewModel.activeSpot().id]);
+    }
+    $('#infoModal').modal('show');
+    // Make sure pictures are loaded before initializing scroll bar
+    window.setTimeout(function() {
+        $('#picture-reel').perfectScrollbar({suppressScrollY: true,
+                                             useBothWheelAxes: true,
+                                             wheelSpeed: 0.5});
+    }, 1000);
+}
+
+function populateDomFsTip() {
+    // Adds Foursquare tip content to the modal
+    var marker = my.viewModel.activeSpot().marker();
+    if (marker.fsReview) {
+        $('#fs-review').html('<h4>Top tip from Foursquare</h4><blockquote class="blockquote-reverse">' +
+            marker.fsReview.text + '<footer><img src="' + marker.fsReview.user.picture +
+            '"><a href="' + marker.fsReview.url + '" target="new">' + marker.fsReview.user.name +
+            '</a></footer></blockquote>');
+    }
+}
+function populateDomFlickr() {
+    // Adds flickr photos to the picture reel
+    var marker = my.viewModel.activeSpot().marker();
+    if (marker.flickr) {
+        $('#flickr').html('<h4>Pictures from flickr</h4><div id="picture-reel"></div>');
+        for (var i = 0; i < marker.flickr.length; i++) {
+            $('#picture-reel').append('<img src="' + marker.flickr[i].url + '" title="' +
+                marker.flickr[i].title + ' by ' + marker.flickr[i].photographer + '">');
+        }
+    } else {
+        // If there are no photos, hide the `#flickr` element
+        $('#flickr').hide();
+    }
+}
+
+function populateDomFsVenueDetails() {
+    // Adds foursquare details to the modal
+    var marker = my.viewModel.activeSpot().marker();
+    // Add rating, if any
+    if (marker.fs_id) {
+        if (marker.fs_rating) {
+            $('#fs-rating').prepend(marker.fs_rating).show();
+        } else {
+            // Hide rating element if no rating
+            $('#fs-rating').hide();
+        }
+
+        // Add address + phone
+        $('#fs-info').html('<h4>Details about ' + marker.title +
+            '</h4><div class="row" id="fs-info-cont"></div>');
+        var addr = '<div class="col-xs-4" id="address-col"><h5>Address:</h5>' +
+            '<address id="fs-address"><strong>' + marker.address[0] + '</strong>';
+        for (var i = 1; i < marker.address.length - 1; i++) {
+            addr += '<br>' + marker.address[i];
+        }
+        if (marker.phone) {
+            addr += '<br>Tel: ' + marker.phone;
+        }
+        addr += '</address>';
+        $('#fs-info-cont').html(addr);
+
+        // Add opening hours
+        if (marker.hours) {
+            $('#fs-info-cont').append('<div class="col-xs-4" id="hours-col"><h5>Opening hours</h5>' +
+                '<dl id="hours"></dl></div>');
+            for (var i = 0; i < marker.hours.timeframes.length; i++) {
+                var time = marker.hours.timeframes[i];
+                $('#hours').append('<dt>' + time.days + '</dt><dd>' +
+                    time.open[0].renderedTime + '</dd>');
+            }
+        }
+
+        // Add links
+        $('#fs-info-cont').append('<div class="col-xs-4" id="links"><h5>Links</h5></div>');
+        if (marker.webpage) { // Venue's homepage
+            $('#links').append('<a href="' + marker.webpage +
+                '" class="btn btn-primary btn-xs btn-block" target="new">Webpage</a>');
+        }
+        if (marker.fs_url) { // Venues foursquare page
+            $('#links').append('<a href="' + marker.fs_url +
+                '" class="btn btn-danger btn-xs btn-block" target="new">Foursquare page</a>');
+        }
+        if (marker.social.facebook) { // Facebook page
+            $('#links').append('<a href="http://facebook.com/' + marker.social.facebook +
+                '" class="btn btn-info btn-xs btn-block" target="new">Facebook page</a>');
+        }
+        if (marker.social.twitter) { // Twitter profile
+            $('#links').append('<a href="http://twitter.com/' + marker.social.twitter +
+                '" class="btn btn-info btn-xs btn-block" target="new">Twitter</a>');
+        }
+        // flickr page
+        $('#links').append('<a href="https://www.flickr.com/search/?text=' + marker.title +
+            '" class="btn btn-success btn-xs btn-block" target="new">flickr</a>');
+        if (marker.wiki) { // Wikipedia page
+            $('#links').append('<a href="' + marker.wiki +
+                '" class="btn btn-warning btn-xs btn-block" target="new">Wikipedia</a>');
+        }
+
+    } else {
+        // Display error message if info could not be found
+        $('#fs-info').html('<h4 class="text-center">Could not get information from Foursquare</h4>');
     }
 }
--- js/map.js
+++ js/map.js
@@ -2,10 +2,11 @@ var map;
 var infoWindow;
 var markers = [];
 
-// Initialize map
 function initMap() {
+    // Initialize map
     map = new google.maps.Map(document.getElementById('map'), {
         mapTypeControl: false,
+        // Hide place labels
         styles: [{"featureType": "poi", "elementType": "labels", "stylers":[{"visibility": "off"}]}]
     });
     
@@ -37,9 +38,8 @@ function initMap() {
     map.fitBounds(bounds);
 }
 
-
-// Add content to info windows
 function populateInfoWindow(marker, infowindow) {
+    // Add content to info windows
     if (infowindow.marker != marker) {
         infowindow.setContent('');
         infowindow.marker = marker;
@@ -51,6 +51,7 @@ function populateInfoWindow(marker, infowindow) {
         }
 
         function setWindowContent() {
+            // Adds content to the infowindow element
             var content = '<h4>' + marker.title + '<br><small>' + marker.category + '</small></h4>';
             content += '<address><strong>' + marker.title + '</strong>';
             for (var i = 0; i < marker.address.length - 1; i++) {
@@ -82,10 +83,13 @@ function populateInfoWindow(marker, infowindow) {
 
 function openInfoWindow(marker) {
     infoWindow.setMap(null);
+    // Animate marker
     marker.setAnimation(google.maps.Animation.BOUNCE);
+    // Make sure the correct active marker is set to highligh it in item list
     if (my.viewModel.activeSpot() != my.viewModel.getSpotById(marker.id)) {
         my.viewModel.setActiveFromMarker(marker.id);
     }
+    // Stop animation and open info window
     window.setTimeout(function() {
         marker.setAnimation(null);
         populateInfoWindow(marker, infoWindow);
@@ -100,117 +104,4 @@ function showMarker(id) {
 
 function hideMarker(id) {
     markers[id].setMap(null);
-}
-
-function openModal() {
-    if (!infoWindow.marker.detailed) {
-        getWiki(infoWindow.marker);
-        getFlickr(infoWindow.marker, populateDomFlickr);
-        getFsTips(infoWindow.marker, populateDomFsTip);
-        getFsVenueDetails(infoWindow.marker, populateDomFsVenueDetails);
-        infoWindow.marker.detailed = true;
-    } else {
-        populateDomFlickr()
-        populateDomFsTip();
-        populateDomFsVenueDetails();
-    }
-    showModal();
-
-    function showModal() {
-        infoWindow.setMap(null);
-        infoWindow.marker = null;
-        if (!my.viewModel.activeSpot().marker()) {
-            my.viewModel.activeSpot().marker(markers[my.viewModel.activeSpot().id]);
-        }
-        $('#infoModal').modal('show');
-        // Make sure pictures are loaded before initializing scroll bar
-        window.setTimeout(function() {
-            $('#picture-reel').perfectScrollbar({suppressScrollY: true,
-                                                 useBothWheelAxes: true,
-                                                 wheelSpeed: 0.5});
-        }, 1000);
-    }
-    function populateDomFsTip() {
-        var marker = my.viewModel.activeSpot().marker();
-        if (marker.fsReview) {
-            $('#fs-review').html('<h4>Top tip from Foursquare</h4><blockquote class="blockquote-reverse">' +
-                marker.fsReview.text + '<footer><img src="' + marker.fsReview.user.picture +
-                '"><a href="' + marker.fsReview.url + '" target="new">' + marker.fsReview.user.name +
-                '</a></footer></blockquote>');
-        }
-    }
-    function populateDomFlickr() {
-        var marker = my.viewModel.activeSpot().marker();
-        if (marker.flickr) {
-            $('#flickr').html('<h4>Pictures from flickr</h4><div id="picture-reel"></div>');
-            for (var i = 0; i < marker.flickr.length; i++) {
-                $('#picture-reel').append('<img src="' + marker.flickr[i].url + '" title="' +
-                    marker.flickr[i].title + ' by ' + marker.flickr[i].photographer + '">');
-            }
-        } else {
-            $('#flickr').hide();
-        }
-    }
-    
-    function populateDomFsVenueDetails() {
-        var marker = my.viewModel.activeSpot().marker();
-        // Add rating
-        if (marker.fs_id) {
-            if (marker.fs_rating) {
-                $('#fs-rating').prepend(marker.fs_rating).show();
-            } else {
-                $('#fs-rating').hide();
-            }
-            // Add address + phone
-            $('#fs-info').html('<h4>Details about ' + marker.title +
-                '</h4><div class="row" id="fs-info-cont"></div>');
-            var addr = '<div class="col-xs-4" id="address-col"><h5>Address:</h5>' +
-                '<address id="fs-address"><strong>' + marker.address[0] + '</strong>';
-            for (var i = 1; i < marker.address.length - 1; i++) {
-                addr += '<br>' + marker.address[i];
-            }
-            if (marker.phone) {
-                addr += '<br>Tel: ' + marker.phone;
-            }
-            addr += '</address>';
-            $('#fs-info-cont').html(addr);
-            // Add opening hours
-            if (marker.hours) {
-                $('#fs-info-cont').append('<div class="col-xs-4" id="hours-col"><h5>Opening hours</h5>' +
-                    '<dl id="hours"></dl></div>');
-                for (var i = 0; i < marker.hours.timeframes.length; i++) {
-                    var time = marker.hours.timeframes[i];
-                    $('#hours').append('<dt>' + time.days + '</dt><dd>' +
-                        time.open[0].renderedTime + '</dd>');
-                }
-            }
-            // Add links
-            $('#fs-info-cont').append('<div class="col-xs-4" id="links"><h5>Links</h5></div>');
-            if (marker.webpage) {
-                $('#links').append('<a href="' + marker.webpage +
-                    '" class="btn btn-primary btn-xs btn-block" target="new">Webpage</a>');
-            }
-            if (marker.fs_url) {
-                $('#links').append('<a href="' + marker.fs_url +
-                    '" class="btn btn-danger btn-xs btn-block" target="new">Foursquare page</a>');
-            }
-            if (marker.social.facebook) {   
-                $('#links').append('<a href="http://facebook.com/' + marker.social.facebook +
-                    '" class="btn btn-info btn-xs btn-block" target="new">Facebook page</a>');
-            }
-            if (marker.social.twitter) {
-                $('#links').append('<a href="http://twitter.com/' + marker.social.twitter +
-                    '" class="btn btn-info btn-xs btn-block" target="new">Twitter</a>');
-            }
-            $('#links').append('<a href="https://www.flickr.com/search/?text=' + marker.title +
-                '" class="btn btn-success btn-xs btn-block" target="new">flickr</a>');
-            if (marker.wiki) {
-                $('#links').append('<a href="' + marker.wiki +
-                    '" class="btn btn-warning btn-xs btn-block" target="new">Wikipedia</a>');
-            }
-        } else {
-            console.log(marker.fs_id);
-            $('#fs-info').html('<h4 class="text-center">Could not get information from Foursquare</h4>');
-        }
-    }
 }
