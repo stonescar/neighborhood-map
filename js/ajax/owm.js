bf5eb53babc2cc405d6483b49f9282a9beb25194
Steinar <stonescar@gmail.com>
2017-08-16 02:01:44 +0200

    use knockout for weather

--- js/ajax/owm.js
+++ js/ajax/owm.js
@@ -2,48 +2,20 @@ function getWeather() {
     // Gets weather data for the next 7 days from Trondheim from OpenWeatherMap.org
     var $w = $('#weather');
     // Only get data if not requested before
-    if ($w.is(':empty')) {
+    if (my.viewModel.weatherForecast().length == 0) {
         var url = 'http://api.openweathermap.org/data/2.5/forecast/daily?id=3133880' +
             '&appid=18749f71cbf50a133071442141f704eb&cnt=7&units=metric';
         $.getJSON(url, function (data) {
             for (var i = 0; i < data.list.length; i++) {
                 var day = data.list[i];
 
-                var date = new Date(day.dt*1000);
-                var weekDay = getWeekDay(date);
-                var fullDate = getFullDate(date);
-                var temp = [Math.round(day.temp.min), Math.round(day.temp.max)];
-                var weather = day.weather[0];
-
-                $w.append('<div class="weather-day"><h5 title="' + fullDate + '">' + weekDay + '</h5>'+
-                    '<img src="http://openweathermap.org/img/w/' + weather.icon + '.png" title="' +
-                    weather.description + '"><span class="weather-temp" title="Min/max temperature (°C)">' + 
-                    temp[0] + '° / ' + temp[1] + '°</div>');
+                my.viewModel.weatherForecast.push(
+                    new Weather(day.dt*1000,
+                                day.temp.min,
+                                day.temp.max,
+                                day.weather[0].description,
+                                day.weather[0].icon));
             }
-
-            // Initializes perfect scrollbar on weather element
-            $('#weather').perfectScrollbar({suppressScrollY: true, useBothWheelAxes: true, wheelSpeed: 0.5});
-
-            function getWeekDay(date) {
-                // Takes a `Date` object and returns the weekday name
-                var weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday',
-                    'Thursday', 'Friday', 'Saturday'];
-                return weekDays[date.getDay()];
-            }
-
-            function getFullDate(date) {
-                // Takes a `Date` object and returns a formatted date string
-                // Format example: "Tue 15. August @ 11:00"
-                var day = date.getDate();
-                var month = date.getMonth();
-                var hour = date.getHours();
-                preDay = getWeekDay(date).substring(0,3);
-                var months = ['January', 'February', 'March', 'April', 'May', 'June',
-                              'July', 'August', 'September', 'October', 'November', 'December'];
-                return preDay + ' ' + day + '. ' + months[month] + ' @ ' + hour + ':00';
-            }
-        }).fail(function(data) {
-            $w.html('<p class="text-center">Could not get weather data</p>');
         });
     }
 }
--- js/app.js
+++ js/app.js
@@ -5,11 +5,39 @@ var Spot = function(data, id, marker) {
 }
 
 
+var Weather = function(date, minTemp, maxTemp, description, icon) {
+    // var self = this;
+    this.date = ko.observable(new Date(date));
+    this.minTemp = ko.observable(Math.round(minTemp));
+    this.maxTemp = ko.observable(Math.round(maxTemp));
+    this.description = ko.observable(description);
+    this.icon = ko.observable(icon);
+    this.weekDay = ko.computed(function() {
+        // Takes a `Date` object and returns the weekday name
+        var weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday',
+            'Thursday', 'Friday', 'Saturday'];
+        return weekDays[this.date().getDay()];
+    }, this);
+    this.fullDate = ko.computed(function() {
+        // Takes a `Date` object and returns a formatted date string
+        // Format example: "Tue 15. August @ 11:00"
+        var day = this.date().getDate();
+        var month = this.date().getMonth();
+        var hour = this.date().getHours();
+        var preDay = this.weekDay().substring(0,3);
+        var months = ['January', 'February', 'March', 'April', 'May', 'June',
+                      'July', 'August', 'September', 'October', 'November', 'December'];
+        return preDay + ' ' + day + '. ' + months[month] + ' @ ' + hour + ':00';
+    }, this);
+}
+
+
 var viewModel = function() {
     var self = this;
     this.spotList = ko.observableArray([]);
     this.activeSpot = ko.observable(null);
     this.filterQuery = ko.observable('');
+    this.weatherForecast = ko.observableArray([]);
 
     // Create spots from `spots.js`
     for (var i = 0; i < spots.length; i++) {
